205895f17a67b69a93c42ab35c5e7f0ac66f8dac
Carlo Carandang <carandangc@gmail.com>
2018-02-11 13:53:20 -0400

    Add BMI view on second screen

--- BMI/app/src/main/java/ca/smu/bmi/Main2Activity.java
+++ BMI/app/src/main/java/ca/smu/bmi/Main2Activity.java
@@ -6,10 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
+    TextView totalTextView;
+    EditText heightTxt;
+    EditText weightTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +30,39 @@ public class Main2Activity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        }); */
+        });
+*/
+        totalTextView = findViewById(R.id.totalTextView);
+        heightTxt = findViewById(R.id.heightTxt);
+        weightTxt = findViewById(R.id.weightTxt);
+
+        Button calcBtn = findViewById(R.id.calcBtn);
+        calcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String value = heightTxt.getText().toString();
+                Float heightAsInt = Float.parseFloat(value);
+
+                String value1 = weightTxt.getText().toString();
+                Float weightAsInt = Float.parseFloat(value1);
+
+                Float calc = (weightAsInt/(heightAsInt*heightAsInt));
+
+                totalTextView.setText(Float.toString((float) calc));
+
+
+
+ /*               Float heightTxt = Float.parseFloat(heightTxt.getText().toString());
+                Float weightTxt = Float.parseFloat(weightTxt.getText().toString());
+*/
+
+            }
+        });
     }
 
 
-    public void calculateBMI(View view){
+/*    public void calculateBMI(View view){
         // gets the height
         EditText height = findViewById(R.id.editText10);
         String value = height.getText().toString();
@@ -47,6 +80,6 @@ public class Main2Activity extends AppCompatActivity {
 
         // use DecimalFormat("0.##") to limit to 2 decimal places
         result.setText((int) calc);
-    }
+    } */
 
 }
